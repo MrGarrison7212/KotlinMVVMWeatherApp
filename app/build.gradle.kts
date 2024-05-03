b52f620dbaedc9b4cee892a80dd8e3a85256a0fc
MrGarrison7212 <bokipfc1992@gmail.com>
2024-05-03 19:37:34 +0200

    Adding some dependencies and plugins pt2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,35 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2.retrofit:2.9.0")
+    implementation("com.squareup.retrofit2.converter-gson:2.9.0")
+
+    //OkHTTP client
+    implementation("com.squareup.okhttp3.okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:2.9.0")
+
+    //lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    //Gson
+
+    implementation("com.google.code.gson:gson:2.10")
+
+    //Additional - adding WeatherView
+
+    implementation("com.github.MatteoBattilana:WeatherView:3.0.0")
+
+    //ViewMode
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+}
+
+kapt {
+    correctErrorTypes = true
 }
