9153033be14a6f70fbd362a796584f5777a0e5e2
MrGarrison7212 <bokipfc1992@gmail.com>
2024-05-13 01:41:24 +0200

    Adding binding - MainActivity.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.kotlinmvvmweatherapp.Activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.kotlinmvvmweatherapp.R
+import com.example.kotlinmvvmweatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            statusBarColor = Color.TRANSPARENT
+        }
+
+        binding.apply {
+            var lat = 51.60
+            var lon = 0.12
+            var name = "London"
+
+            cityTxt.text = name
+
+        }
+
+
     }
 }
