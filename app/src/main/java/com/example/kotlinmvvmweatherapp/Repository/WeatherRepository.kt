0ef454310377a516327eda8d0969d6834c72d50e
MrGarrison7212 <bokipfc1992@gmail.com>
2024-05-27 23:00:08 +0200

    Update WeatherRepository - adding getForecastWeather fun.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Repository/WeatherRepository.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Repository/WeatherRepository.kt
@@ -2,6 +2,7 @@ package com.example.kotlinmvvmweatherapp.Repository
 
 import com.example.kotlinmvvmweatherapp.Server.ApiServices
 import com.example.kotlinmvvmweatherapp.model.CurrentResponseApi
+import com.example.kotlinmvvmweatherapp.model.ForecastResponseApi
 import retrofit2.Call
 
 class WeatherRepository(val api: ApiServices) {
@@ -9,5 +10,9 @@ class WeatherRepository(val api: ApiServices) {
     // adding Call and return value - using call and it functions ( enqueque in MainActivity )
     fun getCurrentWeather(lat : Double, lng : Double, unit : String) : Call<CurrentResponseApi> {
         return api.getCurrentWeather(lat, lng, unit, "eddbf8e49a204cda0a75ffa77a47f0c4")
+    }
+
+    fun getForecastWeather(lat : Double, lng : Double, unit : String) : Call<ForecastResponseApi> {
+        return api.getForecastWeather(lat, lng, unit, "eddbf8e49a204cda0a75ffa77a47f0c4")
     }
 }
