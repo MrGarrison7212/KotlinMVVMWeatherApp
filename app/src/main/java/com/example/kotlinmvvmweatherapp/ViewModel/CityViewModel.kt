7fa555afd3975dd99ff3b60b5365aa645d60569f
MrGarrison7212 <bokipfc1992@gmail.com>
2024-08-30 00:28:05 +0200

    Update UI - Forecast - add constructor to CityViewModel.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/ViewModel/CityViewModel.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/ViewModel/CityViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.kotlinmvvmweatherapp.ViewModel
 
+import androidx.lifecycle.ViewModel
 import com.example.kotlinmvvmweatherapp.Repository.CityRepository
+import com.example.kotlinmvvmweatherapp.Server.ApiClient
+import com.example.kotlinmvvmweatherapp.Server.ApiServices
 
-class CityViewModel(val repository: CityRepository) {
+class CityViewModel(val repository: CityRepository) : ViewModel() {
+    constructor() : this(CityRepository(ApiClient().getClient().create(ApiServices::class.java)))
 }
