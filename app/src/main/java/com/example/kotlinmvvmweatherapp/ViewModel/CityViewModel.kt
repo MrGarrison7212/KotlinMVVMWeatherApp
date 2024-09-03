feb2b9b95e82d871e50aa94ad4ce8ff96459355e
MrGarrison7212 <bokipfc1992@gmail.com>
2024-09-03 22:17:07 +0200

    Update UI - Forecast - add function loadCity.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/ViewModel/CityViewModel.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/ViewModel/CityViewModel.kt
@@ -7,4 +7,7 @@ import com.example.kotlinmvvmweatherapp.Server.ApiServices
 
 class CityViewModel(val repository: CityRepository) : ViewModel() {
     constructor() : this(CityRepository(ApiClient().getClient().create(ApiServices::class.java)))
+
+    fun loadCity(q : String, limit : Int) =
+        repository.getCities(q, limit)
 }
