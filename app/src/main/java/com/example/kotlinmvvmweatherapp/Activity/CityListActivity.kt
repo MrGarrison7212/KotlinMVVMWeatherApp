9886b654d6e0aef3a93967ce36e5d474eb627590
MrGarrison7212 <bokipfc1992@gmail.com>
2024-09-05 01:17:46 +0200

    Update UI - Forecast - add CityListActivity.

---
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinmvvmweatherapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinmvvmweatherapp.R
+
+class CityListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city_list)
+    }
+}
