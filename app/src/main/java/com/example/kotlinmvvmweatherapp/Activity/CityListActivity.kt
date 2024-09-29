272409d3888d93235b889d327eb47fdcb49be2d3
MrGarrison7212 <bokipfc1992@gmail.com>
2024-09-30 00:44:22 +0200

    Update UI - Forecast - add binding - CityListActivity.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
@@ -3,10 +3,14 @@ package com.example.kotlinmvvmweatherapp.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinmvvmweatherapp.R
+import com.example.kotlinmvvmweatherapp.databinding.ActivityCityListBinding
+import com.example.kotlinmvvmweatherapp.databinding.CityViewholderBinding
 
 class CityListActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCityListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_city_list)
+        binding = ActivityCityListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
