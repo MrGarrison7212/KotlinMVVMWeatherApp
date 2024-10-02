f454a6b2953f874eed5fd52ea705d4e534872ad2
MrGarrison7212 <bokipfc1992@gmail.com>
2024-10-03 00:45:22 +0200

    Update UI - Forecast - CityAdapter - update onBindViewHolder.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Adapter/CityAdapter.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Adapter/CityAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.kotlinmvvmweatherapp.Adapter
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.kotlinmvvmweatherapp.Activity.MainActivity
 import com.example.kotlinmvvmweatherapp.databinding.CityViewholderBinding
 import com.example.kotlinmvvmweatherapp.databinding.ForecastViewholderBinding
 import com.example.kotlinmvvmweatherapp.model.CityResponseApi
@@ -30,6 +32,13 @@ class CityAdapter:RecyclerView.Adapter<CityAdapter.ViewHolder>() {
     @SuppressLint("SimpleDateFormat", "SetTextI18n", "DiscouragedApi")
     override fun onBindViewHolder(holder: CityAdapter.ViewHolder, position: Int) {
         val binding = CityViewholderBinding.bind(holder.itemView)
+        binding.root.setOnClickListener {
+            val intent = Intent(binding.root.context, MainActivity::class.java)
+            intent.putExtra("lat", differ.currentList[position].lat)
+            intent.putExtra("lon", differ.currentList[position].lon)
+            intent.putExtra("name", differ.currentList[position].name)
+            binding.root.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount() = differ.currentList.size
