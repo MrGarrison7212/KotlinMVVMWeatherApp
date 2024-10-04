7e7c15c4909ebc6a7ca56b1fa1185afe13268bf7
MrGarrison7212 <bokipfc1992@gmail.com>
2024-10-05 00:59:09 +0200

    Update UI - Forecast - CityListActivity update pt1 - adding cityViewModel.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
@@ -2,13 +2,17 @@ package com.example.kotlinmvvmweatherapp.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.kotlinmvvmweatherapp.Adapter.CityAdapter
 import com.example.kotlinmvvmweatherapp.R
+import com.example.kotlinmvvmweatherapp.ViewModel.CityViewModel
 import com.example.kotlinmvvmweatherapp.databinding.ActivityCityListBinding
 import com.example.kotlinmvvmweatherapp.databinding.CityViewholderBinding
 
 class CityListActivity : AppCompatActivity() {
     lateinit var binding : ActivityCityListBinding
-    private val cityAdapter by lazy { City }
+    private val cityAdapter by lazy { CityAdapter() }
+    private val cityViewModel: CityViewModel by viewModels ()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCityListBinding.inflate(layoutInflater)
