783694771ace473e7a23a7f54112785d18a1a045
MrGarrison7212 <bokipfc1992@gmail.com>
2024-10-08 23:58:45 +0200

    Update UI - Forecast -  update activity_city_list - adding progress bar.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
@@ -29,15 +29,12 @@ class CityListActivity : AppCompatActivity() {
                     count: Int,
                     after: Int
                 ) {
-                    TODO("Not yet implemented")
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    TODO("Not yet implemented")
                 }
 
                 override fun afterTextChanged(s: Editable?) {
-                    TODO("Not yet implemented")
                 }
 
             })
