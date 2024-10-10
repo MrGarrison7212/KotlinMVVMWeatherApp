567ad859d57adf5708e4cf5223f73ac58d0321a9
MrGarrison7212 <bokipfc1992@gmail.com>
2024-10-10 02:11:13 +0200

    Update UI - Forecast -  update activity_city_list - adding progress bar pt2.

--- app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmweatherapp/Activity/CityListActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import com.example.kotlinmvvmweatherapp.Adapter.CityAdapter
@@ -11,6 +12,10 @@ import com.example.kotlinmvvmweatherapp.R
 import com.example.kotlinmvvmweatherapp.ViewModel.CityViewModel
 import com.example.kotlinmvvmweatherapp.databinding.ActivityCityListBinding
 import com.example.kotlinmvvmweatherapp.databinding.CityViewholderBinding
+import com.example.kotlinmvvmweatherapp.model.CityResponseApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CityListActivity : AppCompatActivity() {
     lateinit var binding : ActivityCityListBinding
@@ -35,6 +40,21 @@ class CityListActivity : AppCompatActivity() {
                 }
 
                 override fun afterTextChanged(s: Editable?) {
+                    progressBar3.visibility = View.VISIBLE
+                    cityViewModel.loadCity(s.toString(), 10).enqueue(object :
+                        Callback<CityResponseApi>{
+                        override fun onResponse(
+                            call: Call<CityResponseApi>,
+                            response: Response<CityResponseApi>
+                        ) {
+                            TODO("Not yet implemented")
+                        }
+
+                        override fun onFailure(call: Call<CityResponseApi>, t: Throwable) {
+                            TODO("Not yet implemented")
+                        }
+
+                    })
                 }
 
             })
